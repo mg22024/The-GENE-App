17ee91c0a9c285dfb5140f3e41f3aefa4878be83
mg22024 <74635207+mg22024@users.noreply.github.com>
2024-11-10 20:55:49 -0600

    Add files via upload

---
+++ Gene_App_Menu.py
@@ -0,0 +1,24 @@
+# Program Name: Final_project_The_GENE_App_MG.py.
+# Author: MG
+# Created: 2024-11-10
+# Description: Menu for The Gene App
+# IDE: IDLE
+
+
+# pseudo code
+# Menu
+
+
+
+
+# buttons for the menu
+print('Select what you wish to learn:')
+print('')
+print('1. The Genetic Code')
+print('2. Cells')
+print('3. DNA Model')
+print('4. DNA Base Pairing')
+print('5. DNA  -> mRNA Converter')
+print('6. mRNA -> Proteins Converter')
+print('7. Quiz: Quiz Your Knowledge')
+
